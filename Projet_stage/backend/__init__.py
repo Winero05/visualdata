e3117859c98296c35da4d5b1f82282ba0e8f51bf
Adama-R <adamaadouayom@gmail.com>
2025-11-13 02:25:26 +0000

    Modification du package `backend`.
    Le fichier d'initialisation du backend (backend/__init_.py) a été modifier pour permettre l'exécution des nouvelles modification des modules ainsi qu'à leur tests respectif.

--- Projet_stage/backend/__init__.py
+++ Projet_stage/backend/__init__.py
@@ -1,10 +1,9 @@
-"""Fichier d'initialisation du package backend à ne pas toucher.
+"""Fichier d'initialisation du package ``backend`` à ne pas toucher.
 """
 
 # backend/__init__.py
 
 # Importation des modules minimum pour l'utilisateur
-
 __all__ = [
 	'DataLoader',
 	'CleanDataframeForJson',
@@ -21,18 +20,18 @@ __all__ = [
 	'Visualisation_3D'
 ]
 
-from backend.modules.loading import DataLoader
-from backend.modules.clean_dataframe_for_json import CleanDataframeForJson
-from backend.modules.save_in_data_base import SaveInDataBase
-from backend.modules.config_db import ConfigDb
-from backend.modules.analysis import Analyse
-from backend.modules.netoyage import  Netoyage
-from backend.modules.sansEspace import StringUtils
-from backend.modules.methode_acp import  MethodeACP
-from backend.modules.methode_tsne import  MethodeTSNE
-from backend.modules.methode_umap import  MethodeUMAP
-from backend.modules.auto_selector import  AutoSelector
-from backend.modules.visualisation_2D import Visualisation_2D
-from backend.modules.visualisation_3D import Visualisation_3D
+from modules.loading import DataLoader
+from modules.clean_dataframe_for_json import CleanDataframeForJson
+from modules.save_in_data_base import SaveInDataBase
+from modules.config_db import ConfigDb
+from modules.analysis import Analyse
+from modules.netoyage import  Netoyage
+from modules.sansEspace import StringUtils
+from modules.methode_acp import  MethodeACP
+from modules.methode_tsne import  MethodeTSNE
+from modules.methode_umap import  MethodeUMAP
+from modules.auto_selector import  AutoSelector
+from modules.visualisation_2D import Visualisation_2D
+from modules.visualisation_3D import Visualisation_3D
 
 __version__ = "0.0.1"

da1ee90a6fd7f9f70fd539a872fa08a8b6954f51
Adama-R <adamaadouayom@gmail.com>
2025-11-13 01:56:42 +0000

    Mise à jour du modules `methode_tsne.py`
    Le module `methode_tsne.my` est fonctionnel et peut-être testé avec le fichier `test_methode_tsne.py`.

--- Projet_stage/backend/modules/methode_tsne.py
+++ Projet_stage/backend/modules/methode_tsne.py
@@ -1,14 +1,35 @@
+"""Module de réduction de dimmension avec la méthode t-SNE.
+
+Returns:
+    ndarray: Matrice de données obtenu après réduction avec t-SNE.
+"""
+
 from typing import Union
 import pandas as pd
 import numpy as np
 from sklearn.manifold import TSNE
+from pydantic import ConfigDict
+from pydantic.dataclasses import dataclass
 
+@dataclass(config=ConfigDict(arbitrary_types_allowed=True))
 class MethodeTSNE:
-    def __init__(self, df: Union[pd.DataFrame, np.ndarray, str]):
-        self.df = df
-    
-    def tsne_reduction(self, nombre_de_dimension = 1) -> np.ndarray:
+    """Classe de réduction de dimension avec la méthode t-SNE.
+    """
+
+    df: Union[pd.DataFrame, np.ndarray, str]
+
+    def tsne_reduction(self, nombre_de_dimension: int = 1) -> np.ndarray:
+        """Méthode de réduction de dimension avec t-SNE.
+
+        Args:
+            nombre_de_dimension (int, optional): Le nombre de dimension de réduction. Defaults to 1.
+
+        Returns:
+            np.ndarray: Matrice de données réduite.
+        """
+
         tsne = TSNE(n_components= nombre_de_dimension)
-        X_tsne = tsne.fit_transform(self.df)
-        return X_tsne
-        
+
+        x_tsne = tsne.fit_transform(self.df)
+
+        return x_tsne
--- Projet_stage/backend/tests/modules/test_methode_tsne.py
+++ Projet_stage/backend/tests/modules/test_methode_tsne.py
@@ -1,14 +1,23 @@
-from backend.modules.methode_tsne import MethodeTSNE
-from tests.test_numeric_data import TestNumericData
+"""Test du module `Projet_stage/backend/modules/methode_tsne.py`.
+"""
+
+from modules.methode_tsne import MethodeTSNE
+from tests.modules.test_numeric_data import TestNumericData
 
 class TestMethodeTSNE:
-    def test_methode_tsne(self):
+    """Test de la classe `MethodeTSNE`.
+    """
+
+    def test_methode_tsne(self) -> None:
+        """Test de la méthode `tsne_reduction`.
+        """
+
         chargeur_num_data = TestNumericData()
-        
+
         data = chargeur_num_data.send_numeric_data()
-        
+
         chargeur_tsne = MethodeTSNE(data)
-        
-        X_tsne = chargeur_tsne.tsne_reduction(nombre_de_dimension=2)
-        
-        assert X_tsne is not None
+
+        x_tsne = chargeur_tsne.tsne_reduction(nombre_de_dimension=2)
+
+        assert x_tsne is not None

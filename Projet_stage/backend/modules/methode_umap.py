733451a3efa22b25c9fb9f617800348c3c14e133
Adama-R <adamaadouayom@gmail.com>
2025-11-13 02:06:16 +0000

    Mise à jour du module `methode_umap.py`.
    Le module `methode_umap.py` est fonctionnel et peut-être testé par le fichier `test_methode_umap.py`.

--- Projet_stage/backend/modules/methode_umap.py
+++ Projet_stage/backend/modules/methode_umap.py
@@ -1,14 +1,35 @@
+"""Module de réduction de dimension avec la méthode UMAP.
+
+Returns:
+    np.ndarray: Matrice de réduction de données obtenu grâce à la méthode UMAP.
+"""
+
 from typing import Union
 import pandas as pd
 import numpy as np
 import umap
+from pydantic import ConfigDict
+from pydantic.dataclasses import dataclass
 
+@dataclass(config=ConfigDict(arbitrary_types_allowed=True))
 class MethodeUMAP:
-    def __init__(self, df: Union[pd.DataFrame, np.ndarray, str]):
-        self.df = df
-    
-    def umap_reduction(self, nombre_de_dimension = 1) -> np.ndarray:
+    """Classe de réduction de dimension avec la méthode UMAP.
+    """
+
+    df: Union[pd.DataFrame, np.ndarray, str]
+
+    def umap_reduction(self, nombre_de_dimension: int = 1) -> np.ndarray:
+        """Méthode de réduction de dimension UPAM.
+
+        Args:
+            nombre_de_dimension (int, optional): _description_. Defaults to 1.
+
+        Returns:
+            np.ndarray: Matrice de réduction de données obtenu grâce à la méthode UMAP.
+        """
+
         _umap = umap.UMAP(n_components= nombre_de_dimension)
-        X_umap = _umap.fit_transform(self.df)
-        return X_umap
-        
+
+        x_umap = _umap.fit_transform(self.df)
+
+        return x_umap
--- Projet_stage/backend/tests/modules/test_methode_umap.py
+++ Projet_stage/backend/tests/modules/test_methode_umap.py
@@ -1,14 +1,22 @@
-from tests.test_numeric_data import TestNumericData
-from backend.modules.methode_umap import MethodeUMAP
+"""Test du module `Projet_stage/backend/modules/methode_umap.py`.
+"""
+
+from tests.modules.test_numeric_data import TestNumericData
+from modules.methode_umap import MethodeUMAP
 
 class TestMethodeUMAP:
-    def test_umap_reduction(self):
+    """Test de la classe `MethodeUMAP`.
+    """
+    def test_umap_reduction(self) -> None:
+        """Test de la méthode `umpa_reduction`.
+        """
+
         chargeur_num_data = TestNumericData()
-        
+
         data = chargeur_num_data.send_numeric_data()
-        
+
         chargeur_umap = MethodeUMAP(data)
-        
-        X_umap = chargeur_umap.umap_reduction(nombre_de_dimension=2)
-        
-        assert X_umap is not None
+
+        xumap = chargeur_umap.umap_reduction(nombre_de_dimension=2)
+
+        assert xumap is not None

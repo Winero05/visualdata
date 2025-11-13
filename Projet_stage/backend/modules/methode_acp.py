2a7cadea906cc5510cc71c7184b2bff5831599c4
Adama-R <adamaadouayom@gmail.com>
2025-11-13 01:39:38 +0000

    Mise à jour du module `methode_acp.py`.
    Le module `methode_acp.py` est fonctionnel avec succès et peut-être testé avec le fichier `test_mathode_acp.py`.

--- Projet_stage/backend/modules/methode_acp.py
+++ Projet_stage/backend/modules/methode_acp.py
@@ -1,26 +1,56 @@
+"""Modules de réduction de dimention avec la méthode ACP.
+
+Classes:
+    MethodeACP:
+        Réduction de dimenssion avec l'ACP.
+
+        df: Union[pd.DataFrame, np.ndarray, str]
+
+        Methodes:
+            acp_reduction(self, nombre_dimenssion: int = 1) -> np.ndarray:
+                Méthode de réduction d'ACP
+
+                Args:
+                    nombre_dimenssion (int, optional): Le nombre de dimention de réduction. Defaults to 1.
+
+                Returns:
+                    ndarray: Matrice construite par la méthode d'ACP.
+Returns:
+    ndarray: Matrice construite par la méthode d'ACP.
+"""
+
+from typing import Union
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-from typing import Union
 import numpy as np
 import pandas as pd
+from pydantic import ConfigDict
+from pydantic.dataclasses import dataclass
 
+@dataclass(config=ConfigDict(arbitrary_types_allowed=True))
 class MethodeACP:
     """Réduction de dimenssion avec l'ACP."""
-    
-    def __init__(self, df: Union[pd.DataFrame, np.ndarray, str]):
-        self.df = df
-    
-    def acp_reduction(self, nombre_dimenssion = 1):
 
-            normalisation = StandardScaler()
+    df: Union[pd.DataFrame, np.ndarray, str]
 
-            X_normaliser = normalisation.fit_transform(self.df)
-            
-            acp = PCA(n_components=nombre_dimenssion) # n_components mentionne les axes (dimenssions)
+    def acp_reduction(self, nombre_dimenssion: int = 1) -> np.ndarray:
+        """Méthode de réduction d'ACP
 
-            X_acp = acp.fit_transform(X_normaliser)
+        Args:
+            nombre_dimenssion (int, optional): Le nombre de dimention de réduction. Defaults to 1.
 
-            X_reconstructed = pca.inverse_transform(X_pca)
+        Returns:
+            ndarray: Matrice construite par la méthode d'ACP.
+        """
 
-            return X_reconstructed
+        normalisation = StandardScaler()
 
+        x_normaliser = normalisation.fit_transform(self.df)
+
+        acp = PCA(n_components=nombre_dimenssion) # n_components mentionne les axes (dimenssions)
+
+        x_acp = acp.fit_transform(x_normaliser)
+
+        x_reconstructed = acp.inverse_transform(x_acp)
+
+        return x_reconstructed
--- Projet_stage/backend/tests/modules/test_methode_acp.py
+++ Projet_stage/backend/tests/modules/test_methode_acp.py
@@ -1,20 +1,26 @@
+"""Test du module `Projet_stage/backend/modules/methode_acp.py`
+"""
+
 # Contenue de tests/test_methode_acp.py
 
-from backend.modules.methode_acp import MethodeACP
-import numpy as np
-import pandas as pd
-from tests.test_numeric_data import TestNumericData
+from modules.methode_acp import MethodeACP
+from tests.modules.test_numeric_data import TestNumericData
+
 class TestMethodePCA:
-    def test_pca_reduction(self):
+    """Test de la classe `MethodeACP`.
+    """
+    def test_pca_reduction(self) -> None:
+        """Test de la méthode `pca_reduction`.
+        """
+
         chargeur_num_data = TestNumericData()
-        
+
         data = chargeur_num_data.send_numeric_data()
-        
+
         # Instanciation de la classe `MethodePCA` du fichier `methode_acp.py`.
         chargeur = MethodeACP(data)
-        
-        X_acp = chargeur.acp_reduction(nombre_dimenssion=3)
-        
-        assert X_acp is not None        
-        # assert not X_acp.empty
-    
+
+        x_acp = chargeur.acp_reduction(nombre_dimenssion=3)
+
+        assert x_acp is not None
+        # assert not x_acp.empty

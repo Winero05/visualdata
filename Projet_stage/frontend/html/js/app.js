9eaf253164b3e31b7b63647eec37cd1ab61ac06d
Adama-R <adamaadouayom@gmail.com>
2025-11-28 22:26:54 +0000

    Voici ma position au niveau du frontend.
    Vous pouvez perfectionner le chargement des vues graphique issues de plotly que vous avez en local dans le fichier d'`index.html` et si tout marche bien vous faite un retour sur une nouvelle branche afin qu'on puisse tous l'essayer.

--- Projet_stage/frontend/html/js/app.js
+++ Projet_stage/frontend/html/js/app.js
@@ -1,3 +1,23 @@
+const main_index_html = document.getElementById("main_index_html");
+
+const dashboard = document.getElementById("dashboard");
+
+const open_dashboard_contener = document.getElementById("open_dashboard_contener");
+
+const chargement_de_fichier_contener = document.getElementsByClassName("chargement_de_fichier_contener")[0];
+
+const box_modal = document.getElementsByClassName("box_modal")[0];
+
+const myPopUp = document.getElementsByClassName("myPopUp")[0];
+
+const closePopUp = document.getElementsByClassName("closePopUp")[0];
+
+const type_de_visualisation = document.getElementById("type_de_visualisation");
+
+const infos_data = document.getElementsByClassName("infos_data")[0];
+
+const contener = document.getElementById("contener");
+
 const endpoint = "http://127.0.0.1:8000/v_01/reduction";
 
 const endpoint_data_loading = "http://127.0.0.1:8000/v_01/data_1";
@@ -6,12 +26,10 @@ headers = { "accept": "application/json" };
 
 const btn_path_file = document.getElementsByClassName("btn_path_file")[0];
 
-const box_modal = document.getElementsByClassName("box_modal")[0];
-
 const plot = document.getElementById("plot");
 
 const reduction_methode_options = document.getElementById(
-  "reduction_methode_options"
+    "reduction_methode_options"
 );
 
 const visual_contener = document.getElementsByClassName("visual_contener");
@@ -22,6 +40,56 @@ const visual_3d = document.getElementsByClassName("visual_3d");
 
 const lecteur = new FileReader();
 
+// ------------------------------ open_close_dashbord_contener ---------------------------------
+
+let close_dashboard = true;
+
+open_dashboard_contener.addEventListener("click", (event) => {
+    class_attributs_of_dashboard = dashboard.classList[0];
+    if (close_dashboard) {
+        if (class_attributs_of_dashboard === "open_dashboard") {
+            dashboard.removeAttribute("class");
+        }
+        dashboard.classList.toggle("close_dashboard");
+        dashboard.style.width = "var(--width-dashboard-reduit)";
+        chargement_de_fichier_contener.style.display = "none";
+        type_de_visualisation.style.display = "none";
+        infos_data.style.display = "none";
+        contener.style.width="96.6%";
+        open_dashboard_contener.children[0].style.top="0";
+        open_dashboard_contener.children[0].style.rotate="0 0 0 0rad";
+        open_dashboard_contener.children[1].style.display="block";
+        open_dashboard_contener.children[2].style.top="0";
+        open_dashboard_contener.children[2].style.rotate="0 0 0 0deg";
+        close_dashboard = false;
+    } else {
+        if (class_attributs_of_dashboard === "close_dashboard") {
+            dashboard.removeAttribute("class");
+        }
+        dashboard.classList.toggle("open_dashboard");
+        dashboard.style.width="var(--width-dashboard)";
+        chargement_de_fichier_contener.style.display="flex";
+        type_de_visualisation.style.display="flex";
+        infos_data.style.display="flex";
+        contener.style.width="80%";
+        open_dashboard_contener.children[0].style.top="5px";
+        open_dashboard_contener.children[0].style.rotate="4 0 4 63deg";
+        open_dashboard_contener.children[1].style.display="none";
+        open_dashboard_contener.children[2].style.top="-6.1px";
+        open_dashboard_contener.children[2].style.left="1px";
+        open_dashboard_contener.children[2].style.rotate="4 0 5 -56deg";
+        close_dashboard = true;
+    }
+});
+
+// ---------------------------------------------------------------------------------------------
+
+// ------------------------------ box_modal ---------------------------------
+
+closePopUp.addEventListener("click", (event) => {
+    box_modal.style.display = "none";
+})
+
 btn_path_file.addEventListener("click", () => {
     box_modal.style.display = "flex"; 
     // lecteur.onload = function (event) {
@@ -29,17 +97,37 @@ btn_path_file.addEventListener("click", () => {
     //     afficherCSV(event.target.result);
     // };
     // lecteur.readAsText(path_file.files[0]);
-    visual_contener[0].style.opacity = 1;
+    infos_data.style.cursor = "pointer";
+    infos_data.style.opacity = "1";
+    infos_data.removeAttribute("disabled");
+    // visual_contener[0].style.opacity = "1";
     reduction_methode_options.removeAttribute("disabled");
-    visual_2d[0].removeAttribute("disabled");
-    visual_3d[0].removeAttribute("disabled");
+    // visual_2d[0].removeAttribute("disabled");
+    // visual_3d[0].removeAttribute("disabled");
     reduction_methode_options.style.cursor = "pointer";
-    visual_2d[0].style.cursor = "pointer";
-    visual_3d[0].style.cursor = "pointer";
+    // visual_2d[0].style.cursor = "pointer";
+    // visual_3d[0].style.cursor = "pointer";
     reduction_methode_options.addEventListener('click', (event) => {
         console.log(event.target.value);
     })
 })
+
+// ---------------------------------------------------------------------------
+
+// ------------------------------ infos_data ---------------------------------
+
+infos_data.addEventListener("click", (event) => {
+    const resumer_des_donnees = contener.children[1].classList[0];
+    if (resumer_des_donnees === "resumer_des_donnees") {
+        contener.children[1].removeAttribute("class");
+        contener.children[0].style.height = "88vh";
+    } else {
+        contener.children[1].classList.toggle("resumer_des_donnees");
+        contener.children[0].style.height = "61vh"
+    }
+});
+
+// ---------------------------------------------------------------------------------------------
 
 function afficherCSV(csvText) {
     const lignes = csvText.trim().split("\n");
@@ -61,8 +149,6 @@ function afficherCSV(csvText) {
     plot.innerHTML = ""; // Nettoyer si on recharge un fichier
     plot.appendChild(tableau);
 }
-
-const myPopUp = document.getElementsByClassName("myPopUp")[0];
 
 // console.log(myPopUp[0]);
 

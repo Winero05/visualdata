8e2d992fa5e3d9f12c9a8cd062495dcc6ef62ac3
Adama-R <adamaadouayom@gmail.com>
2025-11-28 22:33:43 +0000

    MAJ du package de test (`__init.py__`).

--- Projet_stage/backend/tests/__init__.py
+++ Projet_stage/backend/tests/__init__.py
@@ -3,30 +3,9 @@
 
 # Package de Projet_stage/backend/tests/__init__.py
 
-# Importation des modules minimum pour l'utilisateur
-
-# __all__ = []
-
-from backend.modules.loading import DataLoader
-from backend.modules.clean_dataframe_for_json import CleanDataframeForJson
-from backend.modules.save_in_data_base import SaveInDataBase
-from backend.modules.config_db import ConfigDb
-from backend.modules.analysis import Analyse
-from backend.modules.netoyage import  Netoyage
-from backend.modules.sansEspace import StringUtils
-from backend.modules.methode_acp import  MethodeACP
-from backend.modules.methode_tsne import  MethodeTSNE
-from backend.modules.methode_umap import  MethodeUMAP
-from backend.modules.auto_selector import  AutoSelector
-from backend.modules.visualisation_2D import Visualisation_2D
-from backend.modules.visualisation_3D import Visualisation_3D
-
-# Version actuelle du travail.
-__version__ = "0.0.1"
-
-# Chemin de test des différents fichier de tests dans Projet_stage/backend/tests/*.py
 # Veuillez personnaliser le chemin suivant pour avoir accès à vos propre données.
-
 ABSOLU = "C:\\Users\\adama\\AllCodeProjets\\visualdata"
 PATH = "\\Projet_stage\\backend\\tests\\data\\csv\\insurance.csv"
 __FILE_PATH_TEST__ = ABSOLU+PATH
+
+PYTHONPATH = "C:/Users/adama/AllCodeProjets/visualdata/Projet_stage/backend/"
